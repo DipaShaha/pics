61b74a7e4e61272a0285d0d4d1929db1df0796a3
DipaShaha <dipashaha92@gmail.com>
2019-05-10 02:31:13 +0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,9 @@ class App extends React.Component{
 
 	onSearchSubmit(term){		
 	 	axios.get('https://api.unsplash.com/',{
+	 		headers:{
+	 			Authorization:'Client-ID 4ba66b873ec0a6622198a22cc8f481eb31af86f6d8021719c3ee1a7ca30bb0d4'
+	 		}
 
 	 	});
 	 	
