2a2f0ac3a3b4802358ea00b49f104f33d8c3b4fa
DipaShaha <dipashaha92@gmail.com>
2019-05-10 22:18:13 +0600

    commit

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,6 +1,13 @@
 import React from 'react';
-const ImageList=(props)=>{
-	console.log(props.images);
-	return <div> </div>
+
+
+
+const ImageList=props=>{
+
+	const images= props.images.map((image)=>{
+		return <img key={image.id} src={image.urls.regular} />
+	});
+	
+	return <div>{images}</div>
 }
 export default ImageList;
